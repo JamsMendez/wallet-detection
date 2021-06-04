c34b0e9984faebc861968dd853bde33ef57dc00e
Jose Angel Mendez Santiago <jamsmendez02@gmail.com>
2021-06-04 00:53:59 -0500

    Aplicacion para transferir Chia

---
+++ README.md
@@ -0,0 +1,26 @@
+==== Wallet Detection ===
+
+# Compilacion
+
+  https://golang.org/dl/
+
+  Descargar e instalar Golang
+
+  > go build -o wallet-detection.exe main.go
+
+# Ejecucion
+
+Agregar el ejecutable en la carpeta
+  C:\Users\YOU_USERS\AppData\Local\chia-blockchain\app-1.1.6\resources\app.asar.unpacked\daemon
+
+Ejecutar desde terminal
+
+  > .\wallet-detection.exe -f TU_FINGERPRINT -address ADDRESS_DESTINO
+  
+
+Ejemplo
+
+  > .\wallet-detection.exe -f 1579837892 -address xch1c6jegev6dtmracpvcg4h69yav5w5anzqzdqgv5j9wylqlpg79afqksfmn6
+
+
+
---
+++ main.go
@@ -0,0 +1,136 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os/exec"
+	"runtime"
+	"strconv"
+	"strings"
+	"time"
+
+	"github.com/fatih/color"
+)
+
+type Amount struct {
+	Value     string
+	Timestamp time.Time
+}
+
+var ID, adddress string
+var nAmount Amount
+
+const totalBalance = "-Total Balance"
+
+func main() {
+	nAmount = Amount{}
+
+	flag.StringVar(&ID, "f", "", "f es el fingerprint")
+	flag.StringVar(&adddress, "address", "", "address es la wallet destino")
+
+	flag.Parse()
+
+	if ID == "" || adddress == "" {
+		fmt.Println("Fingerprint y Address son requeridos")
+
+		return
+	}
+
+	args := []string{
+		"wallet",
+		"show",
+	}
+
+	args = append(args, "-f", ID)
+
+	fmt.Println(color.GreenString(fmt.Sprintf("Fingerprint: %s - Address: %s", ID, adddress)))
+
+	for {
+		var cmd *exec.Cmd
+		if runtime.GOOS == "windows" {
+			cmd = exec.Command(".\\chia.exe", args...)
+		} else {
+			cmd = exec.Command("./chia.exe", args...)
+		}
+
+		stdoutSterr, err := cmd.CombinedOutput()
+		if err != nil {
+			fmt.Println(color.RedString("Wallet.Cmd.Err: "), err)
+			return
+		}
+
+		output := string(stdoutSterr)
+		lines := strings.Split(output, "\n")
+
+		size := len(lines)
+		for i := 0; i < size; i++ {
+			line := lines[i]
+			if strings.Contains(line, totalBalance) {
+				one := strings.Split(line, ":")
+				if len(one) >= 2 {
+					two := strings.Split(one[1], " ")
+					if len(two) >= 2 {
+						value := two[1]
+						transWallet(value)
+					}
+				}
+			}
+		}
+
+		time.Sleep(time.Millisecond * 100)
+	}
+}
+
+func transWallet(value string) {
+	f64, err := strconv.ParseFloat(value, 64)
+	if err != nil {
+		fmt.Println(color.RedString("Wallet.Cmd.Err.XCH: "), err)
+	}
+
+	amount := fmt.Sprintf("%.12f", f64)
+
+	if f64 > 0 && nAmount.Value != amount {
+		args := []string{
+			"wallet",
+			"send",
+			"-a",
+			amount,
+			"-m",
+			"0",
+			"-i",
+			"1",
+			"-t",
+			adddress,
+		}
+
+		fmt.Println(color.YellowString(fmt.Sprint(args)))
+
+		var cmd *exec.Cmd
+		if runtime.GOOS == "windows" {
+			cmd = exec.Command(".\\chia.exe", args...)
+		} else {
+			cmd = exec.Command("./chia.exe", args...)
+		}
+
+		buffer, err := cmd.CombinedOutput()
+		if err != nil {
+			fmt.Println(color.RedString("Wallet.Cmd.Send.Err: "), err)
+		}
+
+		fmt.Println(color.GreenString("Wallet.Send.Ok: "), color.CyanString(string(buffer)))
+
+		nAmount.Value = amount
+	}
+
+	now := time.Now()
+	if !nAmount.Timestamp.IsZero() {
+		diff := now.Sub(nAmount.Timestamp).Seconds()
+		if diff >= 60 {
+			nAmount.Timestamp = now
+			nAmount.Value = ""
+		}
+
+	} else {
+		nAmount.Timestamp = now
+	}
+}
